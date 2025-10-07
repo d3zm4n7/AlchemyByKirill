c159d0d065984f2f5edb444d2f19b717add9770d
opilane Kirill <opilane@HP-CZC2349HTB>
2025-10-07 16:01:06 +0300

    Element implemented

--- Models/Element.cs
+++ Models/Element.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AlchemyByKirill.Models
+namespace AlchemyByKirill.Models;
+
+public class Element
 {
-    internal class Element
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string ImagePath { get; set; } // Путь к иконке, например "fire.svg"
+
+    public Element(int id, string name, string imagePath)
     {
+        Id = id;
+        Name = name;
+        ImagePath = imagePath;
     }
 }
